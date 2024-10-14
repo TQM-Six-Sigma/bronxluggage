162c297d99eff35e5cc33def7ff1416c58c0ce27
TQM-Six-Sigma <tqm6sigma5s@gmail.com>
2024-10-14 23:51:38 +0700

    changeCouter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { LuUser2 } from "react-icons/lu";
-import { useLocation } from 'react-router-dom';
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const location = useLocation();
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem("count");
+    return storedCount !== null ? parseInt(storedCount, 10) : 0;
+  });
 
   useEffect(() => {
-    const countStorage = localStorage.getItem('count');
-    if (countStorage) {
-      setCount(parseInt(countStorage));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('count', count);
+    localStorage.setItem("count", count);
   }, [count]);
 
   useEffect(() => {
     const handleBeforeUnload = () => {
-      setCount((prevCount) => prevCount + 1);
+      setCount(count+1);
     };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
-    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
-  }, []);
-
-  useEffect(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, [location.pathname]);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [count]);
 
   return (
-    <div style={{
-      position: 'absolute',
-      top: 10,
-      right: 20,
-      zIndex: 9999,
-    }}>
-      <span className='text-primary' style={{fontSize: "22px", display: "flex", alignItems: "center"}}>Visited: {count}<LuUser2  style={{fontSize: "20px", marginLeft: "5px"}} /></span>
+    <div
+      style={{
+        position: "absolute",
+        top: 10,
+        right: 20,
+        zIndex: 9999,
+      }}
+    >
+      <span
+        className="text-primary"
+        style={{ fontSize: "22px", display: "flex", alignItems: "center" }}
+      >
+        Visited: {count}
+        <LuUser2 style={{ fontSize: "20px", marginLeft: "5px" }} />
+      </span>
     </div>
   );
 }
